287594e8dd597b94430554cde7de62b0be29dd53
rooban2678 <roobanai01@gmail.com>
2024-01-04 12:09:17 +0530

    program and modulus

---
+++ modulus.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b=2;
+    int c;
+printf("Enter the Value of A");
+scanf("%d",&a);
+c=a%b;
+if (c==0)
+{
+printf("The given value is Even");
+}
+else 
+{
+printf("The given value is Odd");
+}
+return 0;
+}
+
---
+++ program.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+printf("Enter the Value");
+scanf("%d",&a);
+if (a>=0)
+{
+printf("The given value is positive");
+}
+else if (a<0)
+{
+printf("The given value is negative");
+}
+return 0;
+}
+
